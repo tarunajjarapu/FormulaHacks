79eed4766a5ba74be115ec67f76f8ceb51c22f2e
Tarun Ajjarapu <>
2024-03-22 21:29:34 -0500

    Added frontend

--- backend/server.js
+++ backend/server.js
@@ -13,11 +13,6 @@ app.use(express.json())
 app.use(express.urlencoded({ extended : false }))
 
 app.use('/user', require('./routes/userRoutes'))
-app.use('/streak', require('./routes/streakRoutes'))
-app.use('/question', require('./routes/questionRoutes'))
-
-app.get('/', (req, res) => {
-    res.send("<h2>Hi there</h2>")
-})
+app.use('/question', require('./routes/'))
 
 app.listen(port, () => console.log(`Server started on port ${port}`))
