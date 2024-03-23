07f7d795c068028ab6e6cd2b637757799fd7450f
AyujVerma <ayuj.verma@tagmagnet.org>
2024-03-23 07:24:41 -0500

    Front end rahhhhhhh >:)

--- frontend/formula-hacks-app/src/App.js
+++ frontend/formula-hacks-app/src/App.js
@@ -1,9 +1,10 @@
 import MainPage from './MainPage';
-import FoodList from './FoodList'
+import Profile from './Profile';
+import UploadPictureButton from './Slides';
 
 function App() {
   return (
-    <FoodList />
+    <UploadPictureButton />
   );
 }
 
